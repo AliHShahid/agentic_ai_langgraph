114d24d21b075390fefa91f42a85d6c4b5efdde5
Ali Hassan Shahid <alyhassanshahid@gmail.com>
2026-06-21 15:03:15 +0500

    Delete website directory

--- website/website/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Lead
-
-@admin.register(Lead)
-class LeadAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'package', 'company', 'phone', 'submitted_by', 'created_at')
-    search_fields = ('name', 'email')
-    list_filter = ('submitted_by', 'created_at')
